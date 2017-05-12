d9190e59b25169f95e239c4b3ad0274c9022e52b
Rahuld <Rahuld@corp.rpqb.com>
2017-05-12 12:19:57 +0530

    routing issue solved

--- app/controllers/programming.js
+++ app/controllers/programming.js
@@ -29,6 +29,7 @@ export default Ember.Controller.extend({
             var message = this.get('uid');
             var q_type = this.get('q_type');
             var score = this.get('score');
+            var outof = this.get('25')
 
             var model = this.get('model');
             model = model.message;
--- app/routes/test.js
+++ app/routes/test.js
@@ -3,14 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
     model() {
-         this.controllerFor('test').set('loading_image_visibility', 'hide');
+         this.controllerFor('test').set('loading_image_visibility');
        
            /*var message = this.controllerFor('home').get('token');
            console.log('message');
            this.toggleProperty('isShowingModal');
             this.controllerFor('test').set('token', message);*/
 
-             var token = sessionStorage.getItem('token');
+            var token = sessionStorage.getItem('token');
             console.log("Token Fetched: " + sessionStorage.getItem('token'));
             
             if(token === undefined || token === null)
