9bed9d974382367062b98387732cb825932bf3d7
Rahuld <Rahuld@corp.rpqb.com>
2017-05-18 12:51:26 +0530

    acceptance

--- tests/acceptance/home-test.js
+++ tests/acceptance/home-test.js
@@ -3,19 +3,28 @@ import moduleForAcceptance from 'online-test/tests/helpers/module-for-acceptance
 
 moduleForAcceptance('Acceptance | home');
 
-test('visiting /home', function(assert) {
-  visit('/home');
-test('should add Email Id and Password', function(assert){
+test('home page', function (assert) {
   visit('/home');
   andThen(function() {
-assert.equal(currentURL(), '/home');
-});
-});
-});
-test('should redirect to agreenment page after signin', function(assert){
-  visit('/agreement')
-  click(button.signin)
-  andThen(function(){
-    assert.equal(currentURL(), '/agreement')
+    assert.equal(currentURL(), '/home');
   });
+
 });
+
+test('should accept Email Id from user', function (assert) {
+  visit('/home');
+  andThen(function() {
+    assert.equal(currentURL(), '/home');
+  });
+
+});
+
+test('should accept Password from user', function (assert) {
+  visit('/home');
+  andThen(function() {
+    assert.equal(currentURL(), '/home');
+  });
+
+});
+
+
