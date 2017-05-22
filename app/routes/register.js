1098665ed5720e08df3e9ed2bb925b9ed0fde3cf
Piyushap <Piyushap@corp.rpqb.com>
2017-05-22 18:04:27 +0530

    changes in register refreshing

--- app/routes/register.js
+++ app/routes/register.js
@@ -5,7 +5,6 @@ export default Ember.Route.extend({
     model() {
        // console.log(CONFIG);
         this.controllerFor('register').set('loading_image_visibility', 'hide');
-        
         this.controllerFor('register').set('firstname', null);
         this.controllerFor('register').set('lastname', null);
         this.controllerFor('register').set('phonenumber', null);
