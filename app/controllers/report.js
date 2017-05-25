4e2dfa3648142cf802a346145b5894baec85e462
Saivinaym <Saivinaym@corp.rpqb.com>
2017-05-25 09:45:21 +0530

    report page changes

--- app/controllers/report.js
+++ app/controllers/report.js
@@ -30,6 +30,45 @@ columns: [
   
 ],
 
+ actions: {
+
+       /* log_out1 : function(){
+             sessionStorage.setItem('token', null);
+           this.transitionToRoute('home');
+           
+        },*/
+        log_out1: function() {
+
+            //console.log(CONFIG.GOURL);
+            //this.toggleProperty('isShowingModal');
+            //this.set('loading_image_visibility', "show");
+            var mycontroller = this;
+            // var uid;
+             var token = sessionStorage.getItem('token');
+            return $.ajax({
+                url: CONFIG.GOURL + '/logout',
+                type: 'GET',
+                accepts: 'application/json',
+                Authorization: token,
+                success: function(response) {
+                    console.log(JSON.stringify(response));
+                    //uid = response.message;
+                    // mycontroller.set('uid',uid);
+                  //  mycontroller.toggleProperty('isShowingModal');
+                    //mycontroller.set('loading_image_visibility', "hide");
+                    mycontroller.transitionToRoute('programming');
+                    sessionStorage.setItem('token', null);
+                    mycontroller.transitionToRoute('home101');
+
+                },
+                error: function(result) {
+                    console.log('DEBUG: GET Enquiries Failed');
+                }
+            });
+        },
+
+ }
+
 /*actions: {
 
   reportCard : function() {
